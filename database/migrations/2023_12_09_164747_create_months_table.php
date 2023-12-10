954227ea891de8b69b0b59ea5e823f2547145d76
honda-yuu <hondaykayamata@gmail.com>
2023-12-10 03:01:56 +0000

    postテーブルへのuser_idの追加

--- database/migrations/2023_12_09_164747_create_months_table.php
+++ database/migrations/2023_12_09_164747_create_months_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('months', function (Blueprint $table) {
             $table->id();
             $table->string('month', 20);
+            $table->timestamps();
             
         });
     }
