3cb79bf5ffc48f12f343479cd3d0efba0821e7b9
tkokona <koko150325@gmail.com>
2023-12-10 15:13:24 +0900

    投稿編集＆削除機能の実装

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,10 +30,25 @@
                     <img src="{{ $post->image_url }}" alt="画像が読み込めません。"/>
                 </div>
                 <div class="edit"><a href="/posts/{{ $post->id }}/edit">編集</a></div>
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">削除</button> 
+                </form>
             @endforeach
         </div>
         <div>
             {{ $posts->links() }}
         </div>
+        
+        <script>
+            function deletePost(id) {
+                'use strict'
+        
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,8 @@ Route::middleware('auth')->group(function () {
 Route::get('/posts/create', [PostController::class, 'create']);  //投稿フォームの表示
 Route::post('/posts', [PostController::class, 'store']);  //画像を含めた投稿の保存処理
 Route::get('/posts', [PostController::class, 'index']);  //作成した投稿一覧を表示
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 
 require __DIR__.'/auth.php';
